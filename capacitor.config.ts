664def3c85a677e5b175cd20e1d019ed13a5f95a
ru77-ui <rusoruso012@gmail.com>
2026-08-11 09:16:59 +0900

    delete capacitor config

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.timetreeclone',
-  appName: 'TimeTreeClone',
-  webDir: 'public'
-};
-
-export default config;
