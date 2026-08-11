e95c6c740075a3e21e91b4cd5d90603683fa5aa4
ru77-ui <rusoruso012@gmail.com>
2026-08-11 09:42:04 +0900

    fix page path to root

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { auth, db } from "../firebase";
+import { auth, db } from "./firebase";
 import { 
   createUserWithEmailAndPassword, 
   signInWithEmailAndPassword, 
